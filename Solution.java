85e176db4eab24d21aeeda7a07773a693e84e744
Lachezar Kutsarov <lkutsarov@gmail.com>
2020-05-12 12:23:52 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,8 +1,6 @@
-package main.minStepsToPalindrome;
-
 import java.util.Scanner;
 
-public class Challenge {
+public class Solution {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
